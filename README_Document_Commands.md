2c4d6119149c21630e7cfc71b538d2f23f398936
chanM0 <mikec201722@gmail.com>
2018-11-24 11:54:19 -0800

    Made Question -mfr and contract/service flow

--- README_Document_Commands.md
+++ README_Document_Commands.md
@@ -1,3 +1,35 @@
 # Real time single page forum app
 
 ## Made with Pusher, Laravel, VueJs, Vuetify, JWT, and markdown.
+
+---
+
+# BE - Laravel : 01-Make-Migrations
+
+## Make Migrations Branch
+
+-   we need to make migrations for
+    -   questions
+    -   replys
+    -   categories
+    -   likes
+
+### Commands
+
+    - ```php artisan make:model Models/Question -mfr```
+        -  makes model, migration, factory, resource controller.
+    - ``` php artisan make:provider QuestionServiceProvider```
+        - makes a service provider, implemented contract service flow
+            - made a contract, service, and connected them in config/app.php
+
+     - ```php artisan make:model Models/Question -mfr```
+        -  makes model, migration, factory, resource controller.
+    - ``` php artisan make:provider QuestionServiceProvider```
+        - makes a service provider, implemented contract service flow
+            - made a contract, service, and connected them in config/app.php
+
+     - ```php artisan make:model Models/Question -mfr```
+        -  makes model, migration, factory, resource controller.
+    - ``` php artisan make:provider QuestionServiceProvider```
+        - makes a service provider, implemented contract service flow
+            - made a contract, service, and connected them in config/app.php
---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Question;
+use Illuminate\Http\Request;
+
+use App\Contracts\QuestionContract;
+
+class QuestionController extends Controller
+{
+    protected $questionContractRetriever;
+
+    public function __construct(QuestionContract $questionContract)
+    {
+        $this->questionContractRetriever = $questionContract;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return $this->questionContractRetriever->index();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return $this->questionContractRetriever->create();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return $this->questionContractRetriever->store($request);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Question $question)
+    {
+        return $this->questionContractRetriever->show($question);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Question $question)
+    {
+        return $this->questionContractRetriever->edit($question);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Question $question)
+    {
+        return $this->questionContractRetriever->update($request, $question);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Question $question)
+    {
+        return $this->questionContractRetriever->destroy($question);
+    }
+}
