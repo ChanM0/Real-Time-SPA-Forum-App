75778df4293c8da7554c5ac104124d5dc3edfdb8
chanM0 <mikec201722@gmail.com>
2018-11-24 15:06:23 -0800

    finish Delete question  service

--- app/Services/QuestionService.php
+++ app/Services/QuestionService.php
@@ -5,61 +5,64 @@ use Illuminate\Http\Request;
 // use Illuminate\Support\Facades\Auth;
 use App\Models\Question;
 use App\Contracts\QuestionContract;
+use Symfony\Component\HttpFoundation\Response;
 
 class QuestionService implements QuestionContract
 {
-  /**
-   * Display a listing of the resource.
-   *
-   * @return \Illuminate\Http\Response
-   */
-  public function index()
-  {
-    return Question::latest()->get();
-  }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Question::latest()->get();
+    }
 
-  /**
-   * Store a newly created resource in storage.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @return \Illuminate\Http\Response
-   */
-  public function store(Request $request)
-  {
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
         //
-  }
+    }
 
-  /**
-   * Display the specified resource.
-   *
-   * @param  \App\Models\Question  $question
-   * @return \Illuminate\Http\Response
-   */
-  public function show(Question $question)
-  {
-    return $question;
-  }
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Question $question)
+    {
+        return $question;
+    }
 
-  /**
-   * Update the specified resource in storage.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @param  \App\Models\Question  $question
-   * @return \Illuminate\Http\Response
-   */
-  public function update(Request $request, Question $question)
-  {
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Question $question)
+    {
         //
-  }
+    }
 
-  /**
-   * Remove the specified resource from storage.
-   *
-   * @param  \App\Models\Question  $question
-   * @return \Illuminate\Http\Response
-   */
-  public function destroy(Question $question)
-  {
-        //
-  }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Question $question)
+    {
+        $question->delete();
+        // return response('Deleted', Response::HTTP_NO_CONTENT);
+        return response(null, Response::HTTP_NO_CONTENT);
+    }
 }
